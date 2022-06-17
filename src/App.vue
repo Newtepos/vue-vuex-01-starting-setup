926da012ba3b808f0de626185bd1a77a874a99ba
Supakit <supakit.pnm@gmail.com>
2022-06-17 22:54:27 +0700

    Add: TheCounter Cmp

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <base-container title="Vuex">
-    <h3>{{ $store.state.counter }}</h3>
-    <button>Add 1</button>
+    <the-counter></the-counter>
+    <button @click="incrementCounter">Add 1</button>
   </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
 
 export default {
   components: {
     BaseContainer,
+    TheCounter,
   },
+  methods: {
+    incrementCounter() {
+      this.$store.state.counter++;
+    }
+  }
 };
 </script>
 
